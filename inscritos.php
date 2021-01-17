b3d04fe3d1a5bc5511ac14253458b74743193568
marian-star <69377482+marian-star@users.noreply.github.com>
2021-01-16 22:44:54 -0600

    Quitando menu de paginacion de Inscritos

--- inscritos.php
+++ inscritos.php
@@ -248,15 +248,6 @@ echo "<script> $(function (){Swal.fire('Registro exitoso') });</script>";
                         </tbody>
                         
                       </table>
-                      <ul class="table-pagination">
-                        <li><a href="#">Previous</a></li>
-                        <li><a href="#">1</a></li>
-                        <li class="active"><a href="#">2</a></li>
-                        <li><a href="#">...</a></li>
-                        <li><a href="#">8</a></li>
-                        <li><a href="#">9</a></li>
-                        <li><a href="#">Next</a></li>
-                      </ul>
                     </div>
                   </div>
                 </div>
